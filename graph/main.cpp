6f7046c9382e4ba7cb8a7ce758c257b1d1cb01a1
Mason <ab300819@163.com>
2020-06-01 22:28:54 +0800

    refactor: update struct

--- graph/main.cpp
+++ graph/main.cpp
@@ -1,8 +1,8 @@
 //
 // Created by mason on 2020/5/31.
 //
-#include <GLFW/glfw3.h>
 #include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
