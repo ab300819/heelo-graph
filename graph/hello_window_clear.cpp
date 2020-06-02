ed7fb793a8ba0f3d51ef940ab50daa359490ccb3
Mason <ab300819@163.com>
2020-06-02 22:32:47 +0800

    perf: add null check

--- graph/hello_window_clear.cpp
+++ graph/hello_window_clear.cpp
@@ -13,7 +13,14 @@ const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 int main() {
-	glfwInit();
+
+	// 初始化 GLFW
+	if (glfwInit() == GLFW_FALSE) {
+		std::cout << "Failed to initialize GLFW" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	// 配置 GLFW
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -38,7 +45,7 @@ int main() {
 	// ---------------------------------------
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
 		std::cout << "Failed to initialize GLAD" << std::endl;
-		return -1;
+		return EXIT_FAILURE;
 	}
 
 	// render loop
@@ -51,6 +58,8 @@ int main() {
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse
 		// moved etc.)
 		// -------------------------------------------------------------------------------
+		glClearColor(0.2f,0.3f,0.3f,1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
